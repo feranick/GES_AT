92a560aeea24ef32b295c42945847e314efc0733
Nicola Ferralis <feranick@hotmail.com>
2017-08-09 22:23:22 -0400

    Added handling at closing.

--- GES-AT/gridedge_AT_run.py
+++ GES-AT/gridedge_AT_run.py
@@ -3,7 +3,11 @@ import sys
 from PyQt4.QtGui import QApplication
 import gridedgeat
 
-app = QApplication(sys.argv)
-form = gridedgeat.gui.MainWindow()
-form.show()
-app.exec_()
+try:
+    app = QApplication(sys.argv)
+    form = gridedgeat.gui.MainWindow()
+    form.show()
+    app.exec_()
+finally:
+    print("App is closing!")
+
