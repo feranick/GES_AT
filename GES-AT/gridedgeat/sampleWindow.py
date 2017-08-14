bdd251acc636a6ec2cbf4b76ceffbe58717c591d
Nicola Ferralis <feranick@hotmail.com>
2017-08-13 21:43:09 -0400

    Added qt-designer UI for sample configuration.
    It's also converted into python using pyuic5

--- GES-AT/gridedgeat/sampleWindow.py
+++ GES-AT/gridedgeat/sampleWindow.py
@@ -18,9 +18,9 @@ import sys
 from datetime import datetime
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QPushButton, QWidget, QAction,
     QVBoxLayout,QGridLayout,QLabel,QGraphicsView,QFileDialog,QStatusBar,
-    QGraphicsScene,QLineEdit,QMessageBox,QDialog)
+    QGraphicsScene,QLineEdit,QMessageBox,QDialog,QComboBox,QMenuBar)
 from PyQt5.QtGui import (QIcon,QImage,QKeySequence,QPixmap,QPainter)
-from PyQt5.QtCore import (pyqtSlot,QRectF)
+from PyQt5.QtCore import (pyqtSlot,QRectF,QRect,QMetaObject,QCoreApplication)
 
 from . import config
 
@@ -30,10 +30,54 @@ from . import config
 class SampleWindow(QMainWindow):
     def __init__(self):
         super(SampleWindow, self).__init__()
-        self.initUI()
+        self.initUI(self)
     
-    def initUI(self):
-        self.setGeometry(100, 100, 400, 400)
-        self.setWindowTitle('Sample configuration')
+    def initUI(self,MainWindow):
+        MainWindow.resize(800, 600)
+        MainWindow.setWindowTitle('Sample configuration')
         self.statusBar().showMessage("Sample: Ready", 5000)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayoutWidget = QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QRect(30, 10, 751, 531))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout_2 = QGridLayout(self.gridLayoutWidget)
+        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.comboBox = QComboBox(self.gridLayoutWidget)
+        self.comboBox.setObjectName("comboBox")
+        self.gridLayout_2.addWidget(self.comboBox, 0, 1, 1, 1)
+        self.label = QLabel(self.gridLayoutWidget)
+        self.label.setText("label")
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.lineEdit = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout_2.addWidget(self.lineEdit)
+        self.lineEdit_2 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.verticalLayout_2.addWidget(self.lineEdit_2)
+        self.lineEdit_3 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.verticalLayout_2.addWidget(self.lineEdit_3)
+        self.lineEdit_4 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.verticalLayout_2.addWidget(self.lineEdit_4)
+        self.lineEdit_5 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.verticalLayout_2.addWidget(self.lineEdit_5)
+        self.gridLayout_2.addLayout(self.verticalLayout_2, 1, 1, 1, 1)
+        self.label_2 = QLabel(self.gridLayoutWidget)
+        self.label_2.setText("label_2")
+        self.gridLayout_2.addWidget(self.label_2, 1, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
+        QMetaObject.connectSlotsByName(MainWindow)
