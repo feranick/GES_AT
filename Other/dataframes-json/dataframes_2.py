e76cbeae636904e72e832ff51f1d96d9bca7c84b
Nicola Ferralis <feranick@hotmail.com>
2017-08-21 12:18:25 -0400

    Updated dataframe_2.

--- Other/dataframes-json/dataframes_2.py
+++ Other/dataframes-json/dataframes_2.py
@@ -2,25 +2,42 @@ import numpy as np
 import pandas as pd
 import json
 
+
 np0 = np.zeros((0,1))
+print("np0\n",np0)
 np_new0 = np.array(['sample'])
+print(np0.shape, np_new0.shape)
+np0 = np.vstack((np0,np_new0))
 np0 = np.vstack((np0,np_new0))
 print("np_new0\n",np_new0)
 print("np0\n",np0)
 
+np2 = np.zeros((0,4))
+print("np2\n",np2)
+np_new2 = np.array([7,8,9,10])
+print(np2.shape, np_new2.shape)
+print("np_new2\n",np_new2)
+np2 = np.vstack((np2,np_new2))
+np2 = np.vstack((np2,np_new2))
+print("np2\n",np2)
+
+
 np1 = np.zeros((5,2))
 print("np1\n",np1)
 np_new1 = np.array([[1,1],[2,4],[3,9],[4,16],[5,25]])
-print("np_new1\n",np_new1[:,:])
-np1 = np.vstack((np1,np_new1))
+print(np1.shape, np_new1.shape)
+print("np_new1\n",np_new1)
+np1 = np.vstack((np1[:0],np_new1))
+np1 = np.stack((np1,np_new1))
+
 print("np1\n",np1)
+print(np1.shape)
 
-np2 = np.zeros((0,4))
-np_new2 = np.array([7,8,9,10])
-np2 = np.vstack((np2,np_new2))
+print(np1[0])
 
-print("np_new2\n",np_new2)
-print("np2\n",np2)
+
+
+
 
 
 '''
