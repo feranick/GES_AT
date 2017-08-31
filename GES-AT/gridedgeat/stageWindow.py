6754708ece7497f7cec87deb4b25ec1c88518d56
Nicola Ferralis <feranick@hotmail.com>
2017-08-31 00:54:15 -0400

    First implementation of stage buttons

--- GES-AT/gridedgeat/stageWindow.py
+++ GES-AT/gridedgeat/stageWindow.py
@@ -15,7 +15,7 @@ the Free Software Foundation; either version 2 of the License, or
 '''
 
 from PyQt5.QtCore import QRect
-from PyQt5.QtWidgets import (QLabel, QPushButton, QWidget, QMainWindow, QApplication)
+from PyQt5.QtWidgets import (QLabel, QLineEdit, QPushButton, QWidget, QMainWindow, QApplication)
 from .modules.xystage.xystage import *
 
 class StageWindow(QMainWindow):
@@ -24,41 +24,84 @@ class StageWindow(QMainWindow):
         self.initUI(self)
     
     def initUI(self, StageWindow):
-        StageWindow.resize(310, 100)
+        StageWindow.resize(310, 250)
         StageWindow.setWindowTitle("XY stage control settings")
 
         self.stageLabel = QLabel(StageWindow)
         self.stageLabel.setGeometry(QRect(20, 20, 300, 20))
 
         self.homingButton = QPushButton(StageWindow)
-        self.homingButton.setGeometry(QRect(10, 50, 140, 40))
+        self.homingButton.setGeometry(QRect(10, 65, 110, 40))
         self.homingButton.setText("Move Home")
         self.homingButton.clicked.connect(self.moveHome)
         
         self.setCurPositOriginButton = QPushButton(StageWindow)
-        self.setCurPositOriginButton.setGeometry(QRect(160, 50, 140, 40))
+        self.setCurPositOriginButton.setGeometry(QRect(190, 65, 110, 40))
         self.setCurPositOriginButton.setText("Set Origin")
         self.setCurPositOriginButton.clicked.connect(self.setCurrentPosOrigin)
         
+        self.upButton = QPushButton(StageWindow)
+        self.upButton.setGeometry(QRect(120, 50, 70, 70))
+        self.upButton.setText("UP")
+        self.upButton.clicked.connect(lambda: self.moveStage(0,1))
+        self.downButton = QPushButton(StageWindow)
+        self.downButton.setGeometry(QRect(120, 170, 70, 70))
+        self.downButton.setText("DOWN")
+        self.downButton.clicked.connect(lambda: self.moveStage(0,-1))
+        self.leftButton = QPushButton(StageWindow)
+        self.leftButton.setGeometry(QRect(50, 110, 70, 70))
+        self.leftButton.setText("LEFT")
+        self.leftButton.clicked.connect(lambda: self.moveStage(-1,0))
+        self.rightButton = QPushButton(StageWindow)
+        self.rightButton.setGeometry(QRect(190, 110, 70, 70))
+        self.rightButton.setText("RIGHT")
+        self.rightButton.clicked.connect(lambda: self.moveStage(1,0))
+        
+        self.stepStageText = QLineEdit(StageWindow)
+        self.stepStageText.setGeometry(QRect(135, 140, 40, 25))
+        self.stepStageText.setText("0.1")
+        self.stepStageLabel = QLabel(StageWindow)
+        self.stepStageLabel.setGeometry(QRect(140, 115, 40, 25))
+        self.stepStageLabel.setText("Step")
+
         self.xystage = XYstage()
         
         if self.xystage.xystageInit is False:
-            self.homingButton.setEnabled(False)
-            self.setCurPositOriginButton.setEnabled(False)
+            self.enableButtons(False)
             self.stageLabel.setText("XYstage libraries or connection failed")
         else:
             self.stageLabel.setText("XYstage connection OK")
 
+    # Enable/disable buttons and fields
+    def enableButtons(self, flag):
+        self.homingButton.setEnabled(flag)
+        self.setCurPositOriginButton.setEnabled(flag)
+        self.upButton.setEnabled(flag)
+        self.downButton.setEnabled(flag)
+        self.leftButton.setEnabled(flag)
+        self.rightButton.setEnabled(flag)
+        self.stepStageText.setEnabled(flag)
+
+    # Move stage to home position
     def moveHome(self):
         self.stageLabel.setText("Moving stage to home position...")
         QApplication.processEvents()
         self.xystage.move_home()
         self.stageLabel.setText("Ready")
 
-    
+    # Set current position as origin
     def setCurrentPosOrigin(self):
         self.stageLabel.setText("Setting current position as origin")
         QApplication.processEvents()
         self.xystage.set_origin(True, (_,_))
+
+    # Move stage with buttons.
+    # x and y must be either 1, 0, -1
+    def moveStage(self,x,y):
+        step = float(self.stepStageText.text())
+        self.move_rel(x*step, y*step)
+
+
+
 
 
