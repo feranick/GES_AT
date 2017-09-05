dab7fbae7c7dcab40217a89d1afe83b1db633a28
Nicola Ferralis <feranick@hotmail.com>
2017-09-04 22:29:26 -0400

    Experimental testing of Threads for acquisition

--- GES-AT/gridedgeat/acquisition.py
+++ GES-AT/gridedgeat/acquisition.py
@@ -19,6 +19,7 @@ import pandas as pd
 import time, random, math
 from datetime import datetime
 from PyQt5.QtWidgets import (QApplication)
+from PyQt5.QtCore import (QObject, QThread, pyqtSlot, pyqtSignal)
 from .acquisitionWindow import *
 from . import logger
 from .modules.xystage.xystage import *
@@ -390,6 +391,22 @@ class Acquisition():
             obj.resultswind.deviceID,obj.resultswind.perfData,
             obj.resultswind.JV)
     
+    # Experiental use of threads for acqusition JV Acquisition to result page
+    def acqThread(self, obj, obj2, row, column, deviceID, dfAcqParams):
+        my_thread = QThread()
+        my_thread.start()
+
+        # This causes my_worker.run() to eventually execute in my_thread:
+        my_worker = Worker(lambda: self.JVAcq6Devices(self, obj, obj2, row, column, deviceID, dfAcqParams))
+        my_worker.moveToThread(my_thread)
+        my_worker.start.connect(my_worker.run)
+        my_worker.start.emit("hello")
+        # my_worker.finished.connect(self.xxx)
+
+        self.threadPool.append(my_thread)
+        self.my_worker = my_worker
+        del my_thread
+    
     ############  Temporary section STARTS here ###########################
     def generateRandomJV(self):
         VStart = self.dfAcqParams.get_value(0,'Acq Start Voltage')
@@ -444,5 +461,22 @@ class Acquisition():
 
     ############  Temporary section ENDS here ###########################
 
+class Worker(QObject):
+    start = pyqtSignal(str)
+    finished = pyqtSignal()
+
+    def __init__(self, function, *args, **kwargs):
+        super(Worker, self).__init__()
+        #logthread('GenericWorker.__init__')
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        #self.start.connect(self.run)
+
+    @pyqtSlot()
+    def run(self, *args, **kwargs):
+        #logthread('GenericWorker.run')
+        self.function(*self.args, **self.kwargs)
+        self.finished.emit()
 
 
