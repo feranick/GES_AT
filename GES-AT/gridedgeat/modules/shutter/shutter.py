d9a7af20606da41ffc9a61c5fdf9d880f7ca10d9
Nicola Ferralis <feranick@hotmail.com>
2017-10-19 14:06:23 -0400

    Added new module for shutter control via TTL-FTDI

---
+++ GES-AT/gridedgeat/modules/shutter/shutter.py
@@ -0,0 +1,55 @@
+'''
+shutter.py
+-------------
+Class for providing a hardware support for 
+for the the solar Sim shutter via TTL-FTDI
+
+Copyright (C) 2017 Nicola Ferralis <ferralis@mit.edu>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+'''
+
+import ftd2xx
+
+class Shutter():
+    # Define connection to powermeter
+    def __init__(self):
+        try:
+            self.shutter = ftd2xx.open(0)
+            self.shutter.setTimeouts(1000,1000)
+            self.closed()
+        except:
+            Pass
+
+    def __del__(self):
+        try:
+            self.shutter.close()
+        except:
+            pass
+
+    # Open the shutter (0V)
+    def open(self):
+        self.shutter.setRts()
+
+    # Close the shutter (5V)
+    def closed(self):
+        self.shutter.clrRts()
+
+
+
+### This is only for testing - to be removed ###
+if __name__ == '__main__':
+    import time
+    # test
+    shutter = Shutter()
+    time.sleep(1)
+    
+    shutter.open()
+    time.sleep(2)
+    shutter.closed()
+    pass
+
