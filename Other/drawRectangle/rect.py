1ec3e2b3bba2a11642437947dd400552f4e8bb99
Nicola Ferralis <feranick@hotmail.com>
2017-11-01 18:39:32 -0400

    Add drawRectangle example

---
+++ Other/drawRectangle/rect.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5 import QtWidgets, QtCore, QtGui
+#from PyQt5.QtGui import QPainter
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(30,30,600,400)
+        self.begin = QtCore.QPoint()
+        self.end = QtCore.QPoint()
+        self.show()
+
+    def paintEvent(self, event):
+        qp = QtGui.QPainter(self)
+        br = QtGui.QBrush(QtGui.QColor(100, 10, 10, 40))
+        qp.setBrush(br)
+        qp.drawRect(QtCore.QRect(self.begin, self.end))
+
+    def mousePressEvent(self, event):
+        self.begin = event.pos()
+        self.end = event.pos()
+        self.initial = event.pos()
+        self.update()
+
+    def mouseMoveEvent(self, event):
+        self.end = event.pos()
+        self.update()
+
+    def mouseReleaseEvent(self, event):
+        self.begin = event.pos()
+        self.end = event.pos()
+        self.final = event.pos()
+        self.update()
+        print(self.initial.x(), self.initial.y(), self.final.x(), self.final.y())
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWidget()
+    window.show()
+    app.aboutToQuit.connect(app.deleteLater)
+    sys.exit(app.exec_())
