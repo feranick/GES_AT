844bf8bd37280b8ca512752e9f1238d4445c3202
Nicola Ferralis <feranick@hotmail.com>
2017-11-15 20:42:57 -0500

    PhotoView can access image from camera

--- Other/PhotoView/PhotoView.py
+++ Other/PhotoView/PhotoView.py
@@ -7,6 +7,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PIL import Image
 from PIL.ImageQt import ImageQt
+from camera import *
 
 import time
 
@@ -15,28 +16,40 @@ class TestWidget(QWidget):
         QWidget.__init__(self, parent)
         self.scene = QGraphicsScene()
         self.view = QGraphicsView(self.scene)
-        self.button = QPushButton("Do test")
+        self.button = QPushButton("Open image")
+        self.button2 = QPushButton("Get image from camera")
 
         layout = QVBoxLayout()
         layout.addWidget(self.button)
+        layout.addWidget(self.button2)
         layout.addWidget(self.view)
         self.setLayout(layout)
 
         self.button.clicked.connect(self.do_test)
+        self.button2.clicked.connect(self.get_image)
+
 
     def do_test(self):
         img = Image.open('image.png')
+        self.display_image(ImageQt(img))
+        QCoreApplication.processEvents()  # let Qt do his work
+        time.sleep(0.5)
+    
+    def get_image(self):
+        self.cam = CameraFeed()
+        imgage = self.cam.grab_image()
+        img, self.image_data, temp = self.cam.get_image(False,0,0,0,0)
         self.display_image(img)
         QCoreApplication.processEvents()  # let Qt do his work
         time.sleep(0.5)
 
     def display_image(self, img):
         self.scene.clear()
-        w, h = img.size
-        self.imgQ = ImageQt(img)  # we need to hold reference to imgQ, or it will crash
-        pixMap = QPixmap.fromImage(self.imgQ)
+        #w, h = img.size
+        #img = ImageQt(img)  # we need to hold reference to imgQ, or it will crash
+        pixMap = QPixmap.fromImage(img)
         self.scene.addPixmap(pixMap)
-        self.view.fitInView(QRectF(0, 0, w, h), Qt.KeepAspectRatio)
+        self.view.fitInView(QRectF(0, 0, 640, 480), Qt.KeepAspectRatio)
         self.scene.update()
 
 if __name__ == "__main__":
