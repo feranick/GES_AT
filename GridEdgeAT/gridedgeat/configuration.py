54d9736746134013e6f36bf423c84c64d2fd4ba6
Nicola Ferralis <feranick@hotmail.com>
2017-11-18 16:14:06 -0500

    Sync with Autotesting

--- GridEdgeAT/gridedgeat/configuration.py
+++ GridEdgeAT/gridedgeat/configuration.py
@@ -79,7 +79,7 @@ class Configuration():
             'xPosRefCell' : 240,
             'yPosRefCell' : 170,
             'xPosPowermeter' : 240,
-            'yPosPowermeter' : 260,
+            'yPosPowermeter' : 223,
             }
     def defineConfSystem(self):
         self.conf['System'] = {
