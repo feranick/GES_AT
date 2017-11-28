5d66392574b27d813dc971cdb0cf10562b5fe4aa
feranick <feranick@hotmail.com>
2017-11-28 16:40:35 -0500

    Bump to version 0.19.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     package_data={ 'GridEdgeAT': ['gridedgeat/rsrc/*.png',
                       'gridedgeat/modules/switchbox/*.scr','gridedgeat/modules/xystage/APT.*'],},
     include_package_data=True,
-    version='0.19.0',
+    version='0.19.1',
     description='Automated measurements of Current/Voltage profiles for photovoltaic solar cells',
     long_description= """ Measurements of Current/Voltage profiles for photovoltaic solar cells """,
     author='Nicola Ferralis',
