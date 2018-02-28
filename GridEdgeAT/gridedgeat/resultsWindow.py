86f1ff6c6088057baf814b3767591285f445b076
Nicola Ferralis <feranick@hotmail.com>
2018-02-28 11:24:54 -0500

    Cleanup

--- GridEdgeAT/gridedgeat/resultsWindow.py
+++ GridEdgeAT/gridedgeat/resultsWindow.py
@@ -353,7 +353,6 @@ class ResultsWindow(QMainWindow):
     # Logic to save locally devices selected from results table
     def selectDeviceSaveLocally(self, row):
         try:
-            print(self.dfTotDeviceID.iat[0,row])
             self.save_csv(self.dfTotDeviceID.iat[0,row],
                 self.dfTotAcqParams.iloc[[row]],
                 self.dfTotPerfData.iat[0,row],
