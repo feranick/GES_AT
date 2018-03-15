6590e88d529c72184dca54c34ca32c122cb31c88
Nicola Ferralis <feranick@hotmail.com>
2018-03-15 13:00:26 -0400

    Added stand-alone utility to open-close shutter

---
+++ GridEdgeAT/utilities/shutterControl.py
@@ -0,0 +1,107 @@
+'''
+shutterWindow
+------------------
+Class for providing a graphical user interface for 
+manually controlling the shutter
+
+Copyright (C) 2017-2018 Nicola Ferralis <ferralis@mit.edu>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+'''
+import time, sys
+from PyQt5.QtCore import (QRect,QObject, QThread, pyqtSlot, pyqtSignal)
+from PyQt5.QtWidgets import (QLabel, QLineEdit, QCheckBox,
+                             QWidget,QMainWindow,QPushButton,QApplication)
+import ftd2xx
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(None)
+        self.version = "0.1"
+        self.initUI(self)
+    
+    # define UI elements
+    def initUI(self, ShutterWindow):
+        self.setGeometry(10, 610, 340, 100)
+        self.setFixedSize(self.size())
+        
+        self.shutterLabel = QLabel(ShutterWindow)
+        self.shutterLabel.setGeometry(QRect(20, 20, 300, 20))
+        ShutterWindow.setWindowTitle("Shutter controls - version: "+self.version)
+        self.shutterLabel.setText("Ready")
+        self.openShutterButton = QPushButton(ShutterWindow)
+        self.openShutterButton.setGeometry(QRect(10, 50, 150, 40))
+        self.openShutterButton.setText("Open Shutter")
+        self.openShutterButton.clicked.connect(lambda: self.activateShutter(True))
+        self.openShutterButton.setEnabled(True)
+
+        self.closeShutterButton = QPushButton(ShutterWindow)
+        self.closeShutterButton.setGeometry(QRect(170, 50, 150, 40))
+        self.closeShutterButton.setText("Close Shutter")
+        self.closeShutterButton.clicked.connect(lambda: self.activateShutter(False))
+        self.closeShutterButton.setEnabled(False)
+
+    # Activate shutter
+    def activateShutter(self, open):
+        try:
+            shutter = Shutter()
+            if open == True:
+                shutter.open()
+                msg = "Shutter OPEN"
+                self.openShutterButton.setEnabled(False)
+                self.closeShutterButton.setEnabled(True)
+            else:
+                shutter.closed()
+                msg = "Shutter CLOSED"
+                self.openShutterButton.setEnabled(True)
+                self.closeShutterButton.setEnabled(False)
+            time.sleep(0.5)
+        except:
+            msg = "Cannot connect to shutter"
+        self.printMsg(msg)
+
+    # Print status
+    def printMsg(self, msg):
+        self.shutterLabel.setText(msg)
+        print(msg)
+
+    # Close connection upon closing window.
+    def closeEvent(self, event):
+        self.activateShutter(False)
+        
+class Shutter():
+    # Define connection to powermeter
+    def __init__(self):
+        try:
+            self.shutter = ftd2xx.open(0)
+            self.shutter.setTimeouts(1000,1000)
+            self.closed()
+        except:
+            pass
+
+    def __del__(self):
+        try:
+            self.shutter.close()
+        except:
+            pass
+
+    # Open the shutter (0V)
+    def open(self):
+        self.shutter.clrRts()
+
+    # Close the shutter (5V)
+    def closed(self):
+        self.shutter.setRts()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+            
+        
