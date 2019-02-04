8a546c2f656a4fc0c93fcb69d2d8e99054bdc683
Nicola Ferralis <feranick@hotmail.com>
2019-02-04 17:22:16 -0500

    Bump to version 1.4.0

--- GridEdgeAT/gridedgeat/__init__.py
+++ GridEdgeAT/gridedgeat/__init__.py
@@ -11,7 +11,7 @@ the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
 '''
-__version__ = "1.4.x"
+__version__ = "1.4.0"
 __author__ = "<qt><a href = mailto:ferralis@mit.edu> Nicola Ferralis</a></qt>"
 
 from .configuration import *
